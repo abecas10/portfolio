c8ed456c48eba72940a9b0dfb541611eaedae270
albecas10 <104857055+abecas10@users.noreply.github.com>
2025-04-21 15:42:08 +0200

    arreglar boton de download

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ const projectsGrid = document.querySelector('.projects-grid');
                 </div>
                 <!-- Añadimos los botones aquí -->
                 <div class="project-buttons">
-                    <a href="${project.downloadUrl || '#'}" class="btn btn-download" ${project.downloadUrl ? 'download' : ''}>
+                    <a href="${project.downloadUrl || '#'}" class="btn btn-download" target="_blank">
                         <i class="fas fa-download"></i> Try it
                     </a>
                     <a href="${project.sourceCodeUrl || '#'}" class="btn btn-source" target="_blank">
